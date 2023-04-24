aed709de0ea33015fd6a72c3d41c623bf01871a4
rdx420 <padraramesh420@gmail.com>
2023-04-24 11:08:14 +0530

    Violet: Update Vendorsetup
    Co-authored-by: Joker-V2 <nipinna0@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,11 +1,5 @@
-git clone https://github.com/rdx420/T_Vendor_Xiaomi_Violet.git -b thirteen vendor/xiaomi/violet
-git clone https://github.com/SuperiorOS-Devices/kernel_xiaomi_violet.git -b thirteen kernel/xiaomi/violet --depth=1
-git clone https://github.com/kdrag0n/proton-clang.git -b master prebuilts/clang/host/linux-x86/clang-proton --depth=1
+git clone https://github.com/RealN00B/Vendor_Xiaomi_Violet.git --depth=1 -b thirteen vendor/xiaomi/violet
+git clone https://github.com/RealN00B/kernel_xiaomi_violet.git -b thirteen kernel/xiaomi/violet --depth=1
+git clone https://gitlab.com/Joker-V2/prebuilts_clang_host_linux-x86_clang-r487747.git -b master prebuilts/clang/host/linux-x86/clang-r487747 --depth=1
 rm -rf packages/resources/devicesettings
-git clone https://github.com/LineageOS/android_packages_resources_devicesettings -b lineage-19.1 packages/resources/devicesettings
-rm -rf hardware/qcom-caf/sm8150/audio
-rm -rf hardware/qcom-caf/sm8150/display
-rm -rf hardware/qcom-caf/sm8150/media
-git clone https://github.com/SuperiorOS/android_hardware_qcom_media.git -b twelve-caf-sm8150 hardware/qcom-caf/sm8150/media
-git clone https://github.com/SuperiorOS/android_hardware_qcom_audio.git -b thirteen-caf-sm8150  hardware/qcom-caf/sm8150/audio
-git clone https://github.com/SuperiorOS/android_hardware_qcom_display.git -b twelve-caf-sm8150 hardware/qcom-caf/sm8150/display
+git clone https://github.com/LineageOS/android_packages_resources_devicesettings -b lineage-20.0 packages/resources/devicesettings
